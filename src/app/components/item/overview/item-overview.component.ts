7787f53553a1df4522530507e0c26e7155af50b1
Mathias Klieber <mathiasklieber@gmail.com>
2017-12-20 15:10:07 +0100

    add fist things for shopping cart functionality

--- src/app/components/item/overview/item-overview.component.ts
+++ src/app/components/item/overview/item-overview.component.ts
@@ -9,9 +9,24 @@ import { Item } from '../../../../model/item';
 export class ItemOverviewComponent implements OnInit {
 
   items: Item[];
+  selectedItems = new Map<number, CartItem>();
+
   constructor(private itemService: ItemService) { }
 
   ngOnInit() {
     this.itemService.getAll().subscribe(items => this.items = items);
   }
+
+  onItemClick(e: Item) {
+    if (!this.selectedItems.has(e.id)) {
+      this.selectedItems.set(e.id, { item: e, count: 1 });
+    } else {
+      this.selectedItems.get(e.id).count++;
+    }
+  }
+}
+
+class CartItem {
+  item: Item;
+  count: number;
 }
